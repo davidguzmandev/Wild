6b2831bf1ee4a2be91b3fcabb5569284bf664aca
davidruncode <daguz.91@gmail.com>
2025-02-08 22:44:29 -0500

    2nd section

--- src/components/partials/whyus.tsx
+++ src/components/partials/whyus.tsx
@@ -12,7 +12,7 @@ export default function WhyUs({ translations }: { translations: any }) {
       className={`h-screen py-20 transition-all duration-500 ease-out ${
         isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"
       }`}>
-      <div className="max-w-full px-8 sm:px-6 lg:px-20">
+      <div className="max-w-full px-8 sm:px-6 lg:px-20  mb-20">
         <div className="max-w-7xl w-full px-8 sm:px-6 lg:px-8 self-start mb-20">
           <p className="text-sm text-indigo-700 font-semibold tracking-widest uppercase before:content-[''] before:w-16 before:h-[1px] before:bg-gray-600 relative flex items-center gap-8">
             {translations.whyus}
@@ -25,14 +25,14 @@ export default function WhyUs({ translations }: { translations: any }) {
             </p>
           </div>
           <div className="my-10 flex gap-6">
-            <div className="w-2/6 h-96 p-10 bg-cover bg-center" style={{backgroundImage: `url(${Uiux.src})`,}}>
+            <div className="w-2/6 h-[504px] p-10 bg-cover bg-center text-white" style={{backgroundImage: `url(${Uiux.src})`,}}>
               <p className="font-bold text-2xl mb-6">Diseño UI/UX</p>
               <p>
                 Creamos interfaces graficas intuitivas para el usuario con optimizaciones funcionales en cada elemento del producto. 
               </p>
             </div>
             <div className="w-3/6 space-y-6">
-              <div className="h-50 p-10 bg-blue-600">
+              <div className="h-60 p-10 bg-blue-600">
                 <p>Card1</p>
                 <p>
                   Lorem ipsum dolor sit amet consectetur, adipisicing elit.
@@ -41,7 +41,7 @@ export default function WhyUs({ translations }: { translations: any }) {
                   labore maiores perferendis similique!
                 </p>
               </div>
-              <div className="h-40 p-10 bg-blue-600">
+              <div className="h-60 p-10 bg-blue-600">
                 <p>Card1</p>
                 <p>
                   Lorem ipsum dolor sit amet consectetur, adipisicing elit.
@@ -51,7 +51,7 @@ export default function WhyUs({ translations }: { translations: any }) {
                 </p>
               </div>
             </div>
-            <div className="w-1/6 h-96 p-10 bg-blue-600">
+            <div className="w-1/6 h-[504] p-10 bg-blue-600">
                 <p>Card1</p>
                 <p>
                   Lorem ipsum dolor sit amet consectetur, adipisicing elit.
