e31a9cc20f7d2a4296fb2b56f2751ac7090e925a
davidruncode <daguz.91@gmail.com>
2025-02-09 12:48:13 -0500

    2nd section

--- src/components/partials/whyus.tsx
+++ src/components/partials/whyus.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useIntersectionObserver } from "@/hooks/useIntersectionObserver";
 import Uiux from "@/images/uiux.webp";
+import Backgrounduiux from "@/images/backgrounduiux.webp";
+import Image from "next/image";
 
 export default function WhyUs({ translations }: { translations: any }) {
   const isVisible = useIntersectionObserver(0.5, "whyus");
@@ -25,15 +27,24 @@ export default function WhyUs({ translations }: { translations: any }) {
             </p>
           </div>
           <div className="my-10 flex gap-6">
-            <div className="w-2/6 h-[504px] p-10 bg-cover bg-center text-white" style={{backgroundImage: `url(${Uiux.src})`,}}>
+            <div
+              className="w-2/6 h-[504px] p-10 bg-cover bg-center text-white"
+              style={{ backgroundImage: `url(${Backgrounduiux.src})` }}>
               <p className="font-bold text-2xl mb-6">Diseño UI/UX</p>
               <p>
-                Creamos interfaces graficas intuitivas para el usuario con optimizaciones funcionales en cada elemento del producto. 
+                Creamos interfaces graficas intuitivas para el usuario con
+                optimizaciones funcionales en cada elemento del producto.
               </p>
+              <Image 
+                src={Uiux}
+                alt="Diseño UI/UX"
+                width={300}
+                height={300}
+              />
             </div>
             <div className="w-3/6 space-y-6">
               <div className="h-60 p-10 bg-blue-600">
-                <p>Card1</p>
+                <p className="font-bold text-2xl mb-6">Aplicaciones Web</p>
                 <p>
                   Lorem ipsum dolor sit amet consectetur, adipisicing elit.
                   Alias odio dolore eos esse vel repellat corporis sed quas ut,
@@ -52,13 +63,13 @@ export default function WhyUs({ translations }: { translations: any }) {
               </div>
             </div>
             <div className="w-1/6 h-[504] p-10 bg-blue-600">
-                <p>Card1</p>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Alias odio dolore eos esse vel repellat corporis sed quas ut,
-                  exercitationem officia quisquam!
-                </p>
-              </div>
+              <p>Card1</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias
+                odio dolore eos esse vel repellat corporis sed quas ut,
+                exercitationem officia quisquam!
+              </p>
+            </div>
           </div>
         </div>
       </div>
