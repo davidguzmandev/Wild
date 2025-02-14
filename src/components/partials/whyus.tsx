6ac07902e274b0d97cfebbef204f3661470e2668
davidruncode <daguz.91@gmail.com>
2025-02-14 18:11:55 -0500

    3ra Seccion

--- src/components/partials/whyus.tsx
+++ src/components/partials/whyus.tsx
@@ -3,8 +3,11 @@ import { useIntersectionObserver } from "@/hooks/useIntersectionObserver";
 import Uiux from "@/images/uiux.webp";
 import Backgrounduiux from "@/images/backgrounduiux.webp";
 import Backgroundwebapp from "@/images/backgroundwebapp.webp";
+import Backgroundwebsite from "@/images/backgroundwebsite.webp";
+import Backgroundcms from "@/images/backgroundcms.webp";
 import Webapp from "@/images/webapp.webp";
 import Websitedev from "@/images/websitedev.webp";
+import Cms from "@/images/cms.webp";
 import Image from "next/image";
 import { useHandleMouseMove } from "@/hooks/useHandleMouseMove";
 
@@ -24,7 +27,7 @@ export default function WhyUs({ translations }: { translations: any }) {
       className={`min-h-screen my-20 transition-all  ${
         isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"
       }`}>
-      <div className="max-w-full px-8 sm:px-6 lg:px-20  mb-20">
+      <div className="max-w-full px-8 sm:px-6 mb-20">
         <div className="max-w-7xl w-full px-8 sm:px-6 lg:px-8 self-start mb-20">
           <p className="text-sm text-indigo-700 font-semibold tracking-widest uppercase before:content-[''] before:w-16 before:h-[1px] before:bg-gray-600 relative flex items-center gap-8">
             {translations.whyus}
@@ -64,7 +67,7 @@ export default function WhyUs({ translations }: { translations: any }) {
                   transform: `translateX(${position.card1?.x * 6}px) translateY(${position.card1?.y * 6}px)`,
                   transition: "transform 0.3s ease-out",
                 }}
-                className="absolute -bottom-60"
+                className="absolute -bottom-50 xl:-bottom-60 2xl:-bottom-80 3xl:-bottom-100 4xl:-bottom-120"
               />
             </div>
 
@@ -80,7 +83,7 @@ export default function WhyUs({ translations }: { translations: any }) {
                 onMouseLeave={() => resetPosition("card2")}>
                 <div className="p-10 w-3/4">
                   <p className="font-bold text-2xl mb-6">Diseño UI/UX</p>
-                  <p className="text-pretty">
+                  <p className="text-pretty xl:pr-20">
                     Creamos interfaces graficas intuitivas para el usuario con
                     optimizaciones funcionales en cada elemento del producto.
                   </p>
@@ -101,7 +104,7 @@ export default function WhyUs({ translations }: { translations: any }) {
               <div
                 className="h-60 bg-blue-600 bg-cover text-white overflow-hidden"
                 style={{
-                  backgroundImage: `url(${Backgroundwebapp.src})`,
+                  backgroundImage: `url(${Backgroundwebsite.src})`,
                   transform: `rotateX(${position.card3?.y}deg) rotateY(${position.card3?.x}deg)`,
                   transition: "transform 0.3s ease-out",
                 }}
@@ -129,28 +132,28 @@ export default function WhyUs({ translations }: { translations: any }) {
             <div
               className="w-1/6 h-[504] bg-blue-600 bg-cover text-white overflow-hidden"
               style={{
-                backgroundImage: `url(${Backgroundwebapp.src})`,
-                transform: `rotateX(${position.card3?.y}deg) rotateY(${position.card3?.x}deg)`,
+                backgroundImage: `url(${Backgroundcms.src})`,
+                transform: `rotateX(${position.card4?.y}deg) rotateY(${position.card4?.x}deg)`,
                 transition: "transform 0.3s ease-out",
               }}
-              onMouseMove={handleMouseMove("card3")}
-              onMouseLeave={() => resetPosition("card3")}>
+              onMouseMove={handleMouseMove("card4")}
+              onMouseLeave={() => resetPosition("card4")}>
               <div className="p-10 ">
                 <p className="font-bold text-2xl mb-6">CMS</p>
                 <p className="text-wrap">
-                  Wordpress es uno de los mejores, ¿porque no?, trabajamos tambien con Joomla, Drupal, Shopify o incluso Moodle para instituciones educativas.
+                  Wordpress es uno de los mejores, ¿pero porque no?, trabajamos tambien con Joomla, Drupal, Shopify o incluso Moodle para instituciones educativas.
                 </p>
               </div>
               <Image
-                src={Uiux}
+                src={Cms}
                 alt="CMS"
                 width={407}
                 height={407}
                 style={{
-                  transform: `translateX(${position.card3?.x * 4}px) translateY(${position.card3?.y * 2}px)`,
+                  transform: `translateX(${position.card4?.x * 4}px) translateY(${position.card4?.y * 2}px)`,
                   transition: "transform 0.3s ease-out",
                 }}
-                className="absolute -bottom-20 -right-20"
+                className="absolute -bottom-10 -right-10"
               />
             </div>
           </div>
