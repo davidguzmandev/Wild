895b67ab1d6e555712e8077f71a05823ed415cd8
davidruncode <daguz.91@gmail.com>
2025-02-17 19:53:27 -0500

    UI improve

--- src/components/partials/footer.tsx
+++ src/components/partials/footer.tsx
@@ -1,5 +1,7 @@
 "use client";
 import { useIntersectionObserver } from "@/hooks/useIntersectionObserver";
+import Image from "next/image";
+import Logo from "@/images/logo.webp";
 
 export default function Footer({ translations }: { translations: any }) {
   const isVisible = useIntersectionObserver(0.5, "whyus");
@@ -11,22 +13,41 @@ export default function Footer({ translations }: { translations: any }) {
       className={`min-h-screen py-20 transition-all duration-500 ease-out ${
         isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"
       }`}>
-      <div className="max-w-full px-8 sm:px-6 lg:px-20">
-        <div className="mx-32 w-1/3">
-          <p className="text-6xl font-bold uppercase text-stroke">
-            Somos Exelentes en
-          </p>
+      <div className="max-w-full mx-10 md:mx-14 lg:mx-28 md:flex justify-center">
+        <div className="xl:w-1/5 px-10 text-center justify-center grid">
+          <Image
+            src={Logo}
+            alt="Logo Wild"
+            width={300}
+            height={300}
+            className="justify-center"
+          />
+          <p className="text-3xl font-bold">Company</p>
         </div>
-        <div>
-
+        <div className="md:w-2/5">
+          <nav>
+            <ul>
+              <li>Home</li>
+              <li>About Us</li>
+              <li>Services</li>
+              <li>Products</li>
+              <li>Blog</li>
+            </ul>
+          </nav>
         </div>
-        <div>
-          
+        <div className="md:w-2/5">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut magnam,
+          tempora recusandae illum facere repudiandae fugiat expedita explicabo
+          quia minima ducimus libero amet accusamus, provident illo nam
+          necessitatibus iure. Labore.
         </div>
       </div>
 
       <div>
-        
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+        distinctio labore odit esse culpa natus quae quis doloribus reiciendis
+        corrupti modi in assumenda, officiis ipsa ea placeat itaque maxime
+        omnis!
       </div>
     </section>
   );
--- src/components/partials/letstalk.tsx
+++ src/components/partials/letstalk.tsx
@@ -10,18 +10,18 @@ export default function Letstalk({ translations }: { translations: any }) {
     <section
       id="letstalk"
       aria-labelledby="letstalk-heading"
-      className={`min-h-screen py-20 transition-all duration-500 ease-out ${
+      className={`min-h-screen py-20 transition-all duration-500 ease-out flex items-center ${
         isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"
       }`}>
       <div className="max-w-full px-8 sm:px-6 lg:px-20 w-3/4 justify-center m-auto">
         <div className="text-center justify-center">
-          <p className="text-5xl font-base pb-2 tracking-tight">{translations.projectinmind}</p>
-          <p className="text-7xl font-bold pb-10 text-indigo-600">{translations.letstalk}</p>
+          <p className="text-4xl xs:text-5xl font-base pb-2 tracking-tight">{translations.projectinmind}</p>
+          <p className="text-4xl xs:text-7xl font-bold pb-10 text-indigo-600">{translations.letstalk}</p>
           <p className="text-gray-600 pb-8">{translations.letstalkdescription}</p>
           <button
               className="relative gradient-button bg-gradient-to-r from-indigo-500 to-purple-500 font-semibold text-white rounded-full py-4 px-6 group hover:shadow-xl hover:scale-105 transition-all duration-600 uppercase text-lg tracking-tight cursor-pointer"
               aria-label={translations.getintouch}>
-              <span className="inline-flex items-center gap-6">
+              <span className="inline-flex items-center gap-8">
                 {translations.getintouch}{" "}
                 <IconHandFinger
                   stroke={2}
