9e475868a2b97b076b96c6565152981219a3d3f6
davidruncode <daguz.91@gmail.com>
2025-02-18 19:58:39 -0500

    Footer

--- src/components/partials/footer.tsx
+++ src/components/partials/footer.tsx
@@ -2,6 +2,14 @@
 import { useIntersectionObserver } from "@/hooks/useIntersectionObserver";
 import Image from "next/image";
 import Logo from "@/images/logo.webp";
+import {
+  IconMessage2,
+  IconMessageChatbot,
+  IconArrowNarrowUp,
+  IconArrowNarrowUpDashed,
+  IconMailFilled,
+  IconBrandWhatsapp,
+} from "@tabler/icons-react";
 
 export default function Footer({ translations }: { translations: any }) {
   const isVisible = useIntersectionObserver(0.5, "footer");
@@ -10,10 +18,10 @@ export default function Footer({ translations }: { translations: any }) {
     <section
       id="footer"
       aria-labelledby="whyus-heading"
-      className={`min-h-screen py-20 transition-all duration-500 ease-out ${
+      className={`py-10 transition-all duration-500 ease-out ${
         isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"
       }`}>
-      <div className="max-w-full mx-10 md:mx-14 lg:mx-28 md:flex justify-center">
+      <div className="max-w-full mx-10 md:mx-14 lg:mx-28 md:flex justify-center mb-16">
         <div className="xl:w-1/5 px-10 text-center justify-center grid">
           <Image
             src={Logo}
@@ -22,24 +30,57 @@ export default function Footer({ translations }: { translations: any }) {
             height={300}
             className="justify-center"
           />
-          <p className="text-3xl font-bold">Company</p>
         </div>
-        <div className="md:w-2/5">
-          <nav>
+        <div className="md:w-2/5 justify-center flex">
+          <nav className="text-3xl">
             <ul>
-              <li>Home</li>
-              <li>About Us</li>
-              <li>Services</li>
-              <li>Products</li>
-              <li>Blog</li>
+              <li className="pb-6">About Us</li>
+              <li className="pb-6">Services</li>
+              <li className="pb-6">Products</li>
+              <li className="pb-6">Blog</li>
             </ul>
           </nav>
         </div>
+
         <div className="md:w-2/5">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut magnam,
-          tempora recusandae illum facere repudiandae fugiat expedita explicabo
-          quia minima ducimus libero amet accusamus, provident illo nam
-          necessitatibus iure. Labore.
+          <div className="text-xl mb-10">
+            <p>Si tienes dudas, preguntas o solo tener contacto directo con nosotros, envianos un mensaje y estaremos encantados de ponernos en contacto contigo.</p>
+          </div>
+          <div className="gap-4 flex justify-between">
+            <div>
+              <button
+                className="relative gradient-button bg-gradient-to-r from-indigo-500 to-purple-500 font-semibold text-white rounded-full py-4 px-6 group hover:shadow-xl hover:scale-105 transition-all duration-600 uppercase text-lg tracking-tight cursor-pointer"
+                aria-label={translations.getintouch}>
+                <span className="inline-flex items-center gap-8">
+                  {translations.getintouch}{" "}
+                  <IconMessage2
+                    stroke={2}
+                    className="group-hover:hidden group-hover:text-white"
+                  />{" "}
+                  <IconMessageChatbot
+                    stroke={2}
+                    className="hidden group-hover:block group-hover:text-violet-500"
+                  />
+                </span>
+              </button>
+            </div>
+            <div>
+              <button
+                className="bg-gradient-to-r from-indigo-500 to-purple-500 font-semibold text-white rounded-full py-4 px-6 group hover:shadow-xl hover:scale-105 transition-all duration-600 cursor-pointer hover:from-white hover:to-white"
+                aria-label={translations.getintouch}>
+                <span className="inline-flex items-center">
+                  <IconArrowNarrowUp
+                    stroke={2}
+                    className="group-hover:hidden group-hover:text-white"
+                  />{" "}
+                  <IconArrowNarrowUpDashed
+                    stroke={2}
+                    className="hidden group-hover:block group-hover:text-violet-500"
+                  />
+                </span>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
 
--- src/components/partials/letstalk.tsx
+++ src/components/partials/letstalk.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useIntersectionObserver } from "@/hooks/useIntersectionObserver";
-import { IconHandFinger, IconHandClick  } from "@tabler/icons-react";
-
+import { IconHandFinger, IconHandClick } from "@tabler/icons-react";
 
 export default function Letstalk({ translations }: { translations: any }) {
   const isVisible = useIntersectionObserver(0.5, "letstalk");
@@ -15,24 +14,30 @@ export default function Letstalk({ translations }: { translations: any }) {
       }`}>
       <div className="max-w-full px-8 sm:px-6 lg:px-20 w-3/4 justify-center m-auto">
         <div className="text-center justify-center">
-          <p className="text-4xl xs:text-5xl font-base pb-2 tracking-tight">{translations.projectinmind}</p>
-          <p className="text-4xl xs:text-7xl font-bold pb-10 text-indigo-600">{translations.letstalk}</p>
-          <p className="text-gray-600 pb-8">{translations.letstalkdescription}</p>
+          <p className="text-4xl xs:text-5xl font-base pb-2 tracking-tight">
+            {translations.projectinmind}
+          </p>
+          <p className="text-4xl xs:text-7xl font-bold pb-10 text-indigo-600">
+            {translations.letstalk}
+          </p>
+          <p className="text-gray-600 pb-8">
+            {translations.letstalkdescription}
+          </p>
           <button
-              className="relative gradient-button bg-gradient-to-r from-indigo-500 to-purple-500 font-semibold text-white rounded-full py-4 px-6 group hover:shadow-xl hover:scale-105 transition-all duration-600 uppercase text-lg tracking-tight cursor-pointer"
-              aria-label={translations.getintouch}>
-              <span className="inline-flex items-center gap-8">
-                {translations.getintouch}{" "}
-                <IconHandFinger
-                  stroke={2}
-                  className="group-hover:hidden group-hover:text-white"
-                />{" "}
-                <IconHandClick
-                  stroke={2}
-                  className="hidden group-hover:block group-hover:text-violet-500"
-                />
-              </span>
-            </button>
+            className="relative gradient-button bg-gradient-to-r from-indigo-500 to-purple-500 font-semibold text-white rounded-full py-4 px-6 group hover:shadow-xl hover:scale-105 transition-all duration-600 uppercase text-lg tracking-tight cursor-pointer"
+            aria-label={translations.getintouch}>
+            <span className="inline-flex items-center gap-8">
+              {translations.getintouch}{" "}
+              <IconHandFinger
+                stroke={2}
+                className="group-hover:hidden group-hover:text-white"
+              />{" "}
+              <IconHandClick
+                stroke={2}
+                className="hidden group-hover:block group-hover:text-violet-500"
+              />
+            </span>
+          </button>
         </div>
       </div>
     </section>
