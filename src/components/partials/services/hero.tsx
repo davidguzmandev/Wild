dd5849a09550c876ebdcd8cb3ca96265761f65c2
davidruncode <daguz.91@gmail.com>
2025-02-26 19:40:11 -0500

    Services

--- src/components/partials/services/hero.tsx
+++ src/components/partials/services/hero.tsx
@@ -1,6 +1,5 @@
 import { IconCoffee, IconMug } from "@tabler/icons-react";
 import Image from "next/image";
-import Background from "@/images/background.webp";
 import Services from "@/images/services.webp";
 
 interface RandomTextComponentProps {
@@ -16,24 +15,16 @@ export default function Hero({
 }) {
   return (
     <section className="min-h-screen px-2 relative flex flex-col justify-center">
-      <div className="absolute z-0 inset-0">
-        <Image
-          src={Background}
-          alt="background"
-          fill
-          className="object-cover object-center w-full h-full"
-        />
-      </div>
       <div className="justify-center max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative z-10 order-1 max-sm:mt-24">
-        <div className="sm:mt-20 animate-fade-in-down">
+        <div className="sm:mt-20 animate-fade-in-down z-30 relative">
           <div className="flex">
-            <h1 className="text-left font-semibold text-6xl sm:w-1/2">
+            <h1 className="text-left font-semibold text-6xl lg:w-1/2">
               {translations2.turning}{" "}
               <span className="text-indigo-600">{translations2.ideas}</span>{" "}
               {translations2.h1}
             </h1>
           </div>
-          <div className="flex mt-10 ">
+          <div className="flex mt-10">
             <button
               className="relative gradient-button bg-gradient-to-r from-indigo-500 to-purple-500 font-semibold text-white rounded-full py-4 px-6 group hover:shadow-xl hover:scale-105 transition-all duration-600 uppercase text-lg tracking-wide cursor-pointer"
               aria-label={translations.talktous}>
@@ -51,13 +42,8 @@ export default function Hero({
             </button>
           </div>
         </div>
-        <div className="w-24 h-24">
-          <Image 
-          src={Services} 
-          alt="services" 
-          fill 
-          className=""
-          />
+        <div className="absolute lg:-bottom-50 lg:-right-10 z-10">
+          <Image src={Services} alt="services" width={768} height={768} />
         </div>
       </div>
     </section>
