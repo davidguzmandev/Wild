8346e78e83fbe5875009529298aef65bcf71c910
davidruncode <daguz.91@gmail.com>
2025-03-10 19:31:40 -0400

    Services

--- src/components/partials/services/servicecard.tsx
+++ src/components/partials/services/servicecard.tsx
@@ -9,15 +9,27 @@ export default function ServiceCard({ translations }: { translations: any }) {
       image: Web,
       link: "/services/websites",
     },
+    {
+      title: `${translations.listweb.websites}`,
+      description: `${translations.websitestext}`,
+      image: Web,
+      link: "/services/websites",
+    },
+    {
+      title: `${translations.listweb.websites}`,
+      description: `${translations.websitestext}`,
+      image: Web,
+      link: "/services/websites",
+    },
   ];
 
   return (
-    <section className="px-2 flex flex-col justify-center">
+    <section className="px-2 flex flex-wrap justify-center gap-10">
       {cards.map((card, index) => (
         <div
           key={index}
-          className="relative flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
-          <div className="relative mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-indigo-600">
+          className="group relative flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md ">
+          <div className="relative m-4 group-hover:-mt-6 h-40 group-hover:h-50 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-indigo-600 duration-500 ease-in-out">
             <Image
               src={card.image}
               alt={card.title}
@@ -38,7 +50,7 @@ export default function ServiceCard({ translations }: { translations: any }) {
             <button
               data-ripple-light="true"
               type="button"
-              className="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+              className="select-none rounded-lg bg-indigo-600 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-indigo-500/20 transition-all hover:shadow-lg hover:shadow-indigo-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none cursor-pointer">
               Read More
             </button>
           </div>
